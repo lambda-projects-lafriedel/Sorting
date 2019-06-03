e22ada175738703121ae71a19142349ba108b65e
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-03 17:37:22 -0400

    Complete bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,9 +1,9 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+#def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+    #for i in range(0, len(arr) - 1):
+     #   cur_index = i
+      #  smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
              
@@ -15,26 +15,34 @@ def selection_sort( arr ):
 
 
 
-    return arr
+    #return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-    # Loop through all indicies in array
-    for i in range(1, len(arr)):
-        # Create variables for indicies being compared
-        a = arr[i-1]
-        b = arr[i]
-        # If left index is greater than right index, swap places
-        if a > b:
-            a = arr[i]
-            b = arr[i-1]
-        else:
-            pass
+def bubble_sort(arr):
+    # Store length of array to know how many times to iterate
+    iterations = len(arr) - 1
+
+    # Loop through array as long as iterations is 1 or more
+    while iterations > 0:
+
+        # Loop through all indicies in array
+        for i in range(1, len(arr)):
+            left = arr[i-1]
+            right = arr[i]
+
+            # If left index is greater than right index, swap places
+            if arr[i-1] > arr[i]:
+
+                arr[i-1] = right
+                arr[i] = left
+
+        # Decrease iterations by 1
+        iterations -= 1
 
     return arr
 
-
+bubble_sort([29,10,14,37,14])
 # STRETCH: implement the Count Sort function below
 # def count_sort( arr, maximum=-1 ):
 
